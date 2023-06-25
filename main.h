a27bcc1100cb418b48ae4a2035d0fc28a18f9e1b
Rati <og.mogorosi28@gmail.com>
2023-06-25 11:19:01 -0700

    --printf_files

--- main.h
+++ main.h
@@ -8,6 +8,12 @@ int is_digit(char);
 int is_print(char);
 int append_hexa(char, char[], int);
 
+#define BUFF_SIZE 1024
+
+/* Sizesof int */
+#define LONG 2
+#define SHORT 1
+
 /* Print numbers */
 int print_int(va_list types, char buffer[], int flags,
 	int width, int precision, int size);
@@ -21,5 +27,9 @@ int print_hexadecimal(va_list types, char buffer[], int flags,
         int width, int precision, int size);
 int print_hexa_upper(va_list types, char buffer[], int flags,
         int width, int precision, int size);
+
+/* Print chars and string */
+int print_string(va_list type, char buffer[], int flag,
+	int width, int precision, int size);
 
 #endif
